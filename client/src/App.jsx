0a5fde25d738d008694d8913c0e4cec0d17c6d45
Biswarup Ghosh <ghoshbiswarup414@gmail.com>
2024-07-22 03:31:39 +0530

    21

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,13 +21,13 @@ function App() {
     <div className="flex flex-col min-h-screen h-screen">
       <main className="flex-grow">
         <Routes>
-          <Route path="/" element={<Onboarding />} />
-          <Route path="/onboarding2" element={<Onboarding2 />} />
-          <Route path="/onboarding3" element={<Onboarding3 />} />
-          <Route path="https://assignment-work.vercel.app/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/track" element={<ClockPage />} />
-          <Route path="/login-success" element={<LoginSuccess />} />
+          <Route exact path="/" element={<Onboarding />} />
+          <Route exact path="/onboarding2" element={<Onboarding2 />} />
+          <Route exact path="/onboarding3" element={<Onboarding3 />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/track" element={<ClockPage />} />
+          <Route exact path="/login-success" element={<LoginSuccess />} />
         </Routes>
       </main>
     </div>
